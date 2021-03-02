d5624fd7d98a86f3fd0d3d6a28a8570ca5af8bd6
suryamahato4444 <surya4444>
2021-03-02 09:45:35 +0545

    initial commit

---
+++ python_lab_1/lab3_.py
@@ -0,0 +1,14 @@
+'''given the integer N-the number of minutes that is passed mid night
+.how many hours and minutes are displayed on the 24 hour digital clock?
+The program should print two numbers:the numbers of hour(between 0 and 23)
+and the number of minutes (between 0 and 59)'''
+#hour=int(input("Enter the hour passed mid night: "))
+#minute=int(input("Enter the minute passed :"))
+
+#print("the hour and minutes displayed is",hour,'and',minute)
+N=int(input("Enter the min passed mid night:"))
+hours=(N//60)
+minutes=(N%60)
+print(f"The hours is {hours}")
+print(f"the minutes is{minutes}")
+print(f"its {hours} and {minutes}")
---
+++ python_lab_1/lab_area.py
@@ -0,0 +1,5 @@
+#write a program that reads the length of the base and the height of the right angled tringle and points the area
+length=float(input("Enter the length of triangle:"))
+breadth=float(input("Enter the breadth of triangle:"))
+area=1/2*(length*breadth)
+print("The area of the given right angled triangle is",area)
