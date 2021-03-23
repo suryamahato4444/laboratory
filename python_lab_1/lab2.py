1bafedf7f1f423a87c05a3b0f19c3494655c6970
suryamahato4444 <surya4444>
2021-03-23 09:21:10 +0545

    returntype

---
+++ python_lab_1/lab2.py
@@ -0,0 +1,16 @@
+def sum():
+
+    c=a+b
+    return c
+    # print(f"The sum of {a} and {b} ic {c}")
+#parameter are the value that taken from outside
+
+    #print("Program is not over")
+a=int(input("Enter the first number :"))
+b=int(input("Enter the second number :"))
+# print(d)
+sum()
+# print(sum())
+d=sum()
+print(d)
+#print("Run the program")
